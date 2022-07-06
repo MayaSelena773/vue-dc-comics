a91069610809df8e37461ef3e3c8b24a0c62d37b
MayaSelena773 <maya.bono721@gmail.com>
2022-07-06 21:50:56 +0200

    header begin

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
+    <PageHeader />
+      <!--<main>
+
+      </main>
+
+      <Footer>-->
   </div>
 </template>
 
 <script>
+import PageHeader from './components/PageHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  }
-}
+    PageHeader
+  },
+};
 </script>
 
 <style>
-
+@import './style/common.scss';
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <header>
+        <!--Header logo-->
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo">
+        </div>
+
+        <!--Header nav-->
+        <nav>
+            <ul class="flex">
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </nav>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name:'PageHeader'
+}
+</script>
+
+<style scoped>
+
+header {
+    display: flex;
+    justify-content: space-between;
+}
+
+header img {
+    width: 70px;
+    display: inline-block;
+}
+
+.logo {
+    padding: 1rem;
+}
+
+nav {
+    padding: 1rem;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+    padding: 20px;
+}
+
+.flex {
+    display: flex;
+}
+
+</style>
+
