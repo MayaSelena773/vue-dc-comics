856f8f6aad4fe352c548342d509bec9ba53eaaab
MayaSelena773 <maya.bono721@gmail.com>
2022-07-07 16:45:46 +0200

    banner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <PageHeader />
     <MainContent />
+    <PageBanner />
       <!--<Footer>-->
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script>
 import PageHeader from './components/PageHeader.vue';
 import MainContent from './components/MainContent.vue';
+import PageBanner from './components/PageBanner.vue';
 
 export default {
   name: 'App',
   components: {
     PageHeader,
-    MainContent
+    MainContent,
+    PageBanner
   },
 };
 </script>
---
+++ src/components/PageBanner.vue
@@ -0,0 +1,69 @@
+<template>
+    <section class="banner">
+        <div class="flex">
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="dc"> 
+            <p>DIGITAL COMICS</p>
+        </div>
+
+        <div class="flex">
+            <img src="../assets/img/buy-comics-merchandise.png" alt="buycomics"> 
+            <p>DC MERCHANDISE</p>
+        </div>
+
+        <div class="flex">
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="sub"> 
+            <p>SUBSCRIPTION</p>
+        </div>
+
+        <div class="flex">
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="shop" class="little-img"> 
+            <p>COMIC SHOP LOCATOR</p>
+        </div>
+
+        <div class="flex">
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="visa"> 
+            <p>PC POWER VISA</p>
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {
+    name:'PageBanner'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+.flex {
+    display: flex;
+    align-items: center;
+    margin: 10px;
+}
+
+.banner {
+    background-color: $brand_secondary_color;
+    display: flex;
+    justify-content: center;
+
+    img {
+        width: 50px;
+        display: inline-block;
+        margin-right: 5px;
+    }
+
+    .little-img {
+    width: 30px;
+    }
+
+    p {
+        color: white;
+    }
+}
+
+</style>
+
+
+
